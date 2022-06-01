c786e747b54d53ab09acf5d33201b026bebabc3c
mokrooussovi <ivan.mokrooussov@dcmail.ca>
2022-06-01 10:54:58 -0400

    added step2 file to the project

---
+++ StepTwo/Scripts/app.js
@@ -0,0 +1,11 @@
+// IIFE -- Immediately Invoked Function Expression
+// AKA -- Anonymous Self-Executing Function
+(function()
+{
+    function Start()
+    {
+        console.log("App started! Welcome to WEBD6201!");
+    }
+
+    window.addEventListener("load", Start);
+})();
