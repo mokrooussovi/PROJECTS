ad7aebbc4df71eb9f4e505bf489432254e3d8e7b
mokrooussovi <ivan.mokrooussov@dcmail.ca>
2022-06-01 18:16:22 -0400

    added assets folder and update app.css file

--- StepTwo/Scripts/app.js
+++ StepTwo/Scripts/app.js
@@ -2,9 +2,57 @@
 // AKA -- Anonymous Self-Executing Function
 (function()
 {
+    function DisplayHomePage()
+    {
+
+        // Variable for text content to be displayed on page
+        let paragraph1 = [`Welcome to our site!\n
+                    The website demonstrates Live Site setup and DOM Manipulation \.\n
+                    Let\`s start!\n`];
+        let pageHeader = `Welcome Message:`;
+        let titlePage = `Welcome to Group3 Lab 1 Site!`;
+
+        console.log("Home Page");
+ 
+
+        // TO-DO: adjust to insert into div w/in main instead of outside (maybe...)
+        // Target the main element to start inserting new content in
+        let MainContent = document.body.getElementsByClassName('container')[0];
+        let ImgContent = document.body.getElementsByTagName('img')[0];
+
+        // Create a new paragraph element to insert text in
+        let ParaOne = document.createElement("p");
+        ParaOne.setAttribute("id", "ParaOne");
+        ParaOne.setAttribute("class", "mt-3");
+        
+        // Build the paragraph by adding the string array together
+        ParaOne.textContent = paragraph1;
+        ParaOne.innerHTML += ParaOne.textContent;
+
+        // Insert new elements onto document
+        MainContent.insertBefore(ParaOne, ImgContent);
+        SetTitle(titlePage, MainContent);
+        SetHeading(pageHeader, MainContent);
+        
+    }
+
+ 
+
+    // named function option
     function Start()
     {
-        console.log("App started! Welcome to WEBD6201!");
+        console.log("App Started!");
+
+   
+
+        switch(document.title)
+        {
+            case "Home":
+                DisplayHomePage();
+                break;
+ 
+        }
+       
     }
 
     window.addEventListener("load", Start);
